63697ebfcc2c2715b893b8b3ad9ad6050982b371
Todd <todd@Todds-MacBook-Pro.local>
2019-01-31 16:08:03 -0500

    Initial commit

---
+++ scripts/0.942b4d27a74a18811bdc.js
@@ -0,0 +1,240 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{"5SAi":function(a,s,i){},"GuD+":function(a,s,i){"use strict";i.r(s),i.d(s,"render_core",function(){return d});i("YEPY"),i("5SAi");var e=i("lG0c"),l=i("EVdn"),t=i.n(l);function d(){Object(e["bind"])(document.body)`
+    <div class="bello-root section__work">
+        <div class="bello-header">
+            <a href="#"><div class="bello-logo">Todd Oh</div></a>
+            <ul class="bello-menu">
+                ${[{id:"nav-section-work",name:"work",path:"work"},{id:"nav-section-about",name:"about",path:"about"}].map(a=>`\n                    <li data-sectionid="${a.id}"><a href="#${a.path}">${a.name}</a></li>\n                `)}
+            </ul>
+        </div>
+
+        <div class="bello-workdetails">
+            <p class="workdetails-header code">code work</p>
+            <ul class="workdetails code">
+                <li class="tiaa">
+                    <div class="detail-info">
+                        <div class="detail-header">
+                            <p class="title">THISISALLABOUT</p>
+                            <p>An online journalism with data-driven analysis on social issues</p>
+                        </div>
+                    </div>
+
+                    <ul class="detail-codelist">
+                        <li>
+                            <p class="codelist-title">News aggregation algorithm</p>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Python</span>
+                                <span class="desc-lang">MongoDB</span>
+                                <span class="desc-component">Aggregates news stories and cleaning story data for further processing.</span>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="https://thisisallabout.com/theyear/" target="_blank"><p class="codelist-title">Unsupervised  data clustering algorithm ➝</p></a>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Python</span>
+                                <span class="desc-component">Hash-Transform structure with dimension reduction, automatic cluster size detection, and post-processing.</span>
+                            </div>
+                        </li>
+
+                        <li>
+                        <a href="https://thisisallabout.com/whattrumpsaid/" target="_blank"><p class="codelist-title">Clustering work of President Trump's tweet data ➝</p></a>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Python</span>
+                                <span class="desc-component">TfIdfVectorizer with dimension reduction, automatic cluster size detection, and editorial editing via in-house data console.</span>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="https://thisisallabout.com" target="_blank"><p class="codelist-title">Website and visualization contents ➝</p></a>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Javascript</span>
+                                <span class="desc-lang">CSS</span>
+                                <span class="desc-component">Website built with lit-html/es6/webpack, visualization contents rendered using CSS.</span>
+                            </div>
+                        </li>
+
+                        <li>
+                            <p class="codelist-title">In-house data editing console</p>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Javascript</span>
+                                <span class="desc-component">Built with lit-html/es6/webpack, contains dataset file post-processing and editorial tracker.</span>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nsr">
+                    <div class="detail-info">
+                        <div class="detail-header">
+                            <p class="title">#Newsrooms</p>
+                            <p>News aggregation/recommendation app based on A.I. and natural language processing</p>
+                        </div>
+                        <div class="detail-github"></div>
+                    </div>
+
+                    <ul class="detail-codelist">
+                        <li>
+                            <a href="https://github.com/brklynfivefive/swoosh_backend" target="_blank"><p class="codelist-title">News aggregation algorithm ➝</p><a/>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Node.js</span>
+                                <span class="desc-lang">MongoDB</span>
+                                <span class="desc-component">Aggregates news stories, extract keywords, and calculate tf-idf score for further processing.</span>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="https://github.com/brklynfivefive/swoosh_backend" target="_blank"><p class="codelist-title">Unsupervised news briefing algorithm ➝</p></a>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Node.js</span>
+                                <span class="desc-component">Generates a news briefing content using token cross-comparison (n-gram) method and keyword token chaining.</span>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="https://github.com/brklynfivefive/swoosh_ui" target="_blank"><p class="codelist-title">Hybrid mobile applications ➝</p></a>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Javascript</span>
+                                <span class="desc-lang">CSS</span>
+                                <span class="desc-lang">Swift 3</span>
+                                <span class="desc-lang">Java</span>
+                                <span class="desc-component">Built a custom interface framework using Javascript and CSS, chained with native code bridging. (Not based on Phonegap/Xamarin/ReactNative)</span>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="adventure">
+                    <div class="detail-info">
+                        <div class="detail-header">
+                            <p class="title">Adventure/Poplar/Toast</p>
+                            <p>Hangout app with adaptive nearby notification and privacy in mind</p>
+                        </div>
+                        <div class="detail-github"></div>
+                    </div>
+
+                    <ul class="detail-codelist">
+                        <li>
+                            <a href="https://github.com/brklynfivefive/infinity/tree/master/ios_android_native/toast-ios" target="_blank"><p class="codelist-title">Low-power geofencing algorithm ➝</p></a>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Objective-C</span>
+                                <span class="desc-component">Tracks geolocation movements using virtually-created geofences towards latest movement direction, automatic hibernation if stays in geofence regions.</span>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="https://github.com/brklynfivefive/infinity/tree/master/ios_android_native/toast-ios" target="_blank"><p class="codelist-title">Advanced camera with dynamic photo/video scribble feature ➝</p></a>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Objective-C</span>
+                                <span class="desc-component">Interactive camera with tap-or-press capture experience, real-time media filter and scribble functionalities binded to custom web interface framework.</span>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="https://github.com/brklynfivefive/infinity/tree/master/Infinity_webUI" target="_blank"><p class="codelist-title">Hybrid mobile applications ➝</p></a>
+                            <div class="codelist-desc">
+                                <span class="desc-lang">Javascript</span>
+                                <span class="desc-lang">CSS</span>
+                                <span class="desc-lang">Objective-C</span>
+                                <span class="desc-lang">Java</span>
+                                <span class="desc-component">Built a custom interface framework using Javascript and CSS, binded to native camera, media scribble, map, dual-swipeable view, and geo-engine experiences. (Not based on Phonegap/Xamarin/ReactNative)</span>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+            <p class="workdetails-header">design work</p>
+            <a href="files/DesignPortfolio_All_July2018_r0.pdf"><p class="workdetails-header dl-designpf">download design portfolio pdf ➝</p></a>
+            <ul class="workdetails design">
+                <li class="poplar">
+                    <div class="detail-info">
+                        <div class="detail-logo"></div>
+                        <div class="detail-text">
+                            <p class="title">Poplar/Toast</p>
+                            <p>Social media app lets you see and share status only with nearby friends</p>
+                        </div>
+                    </div>
+
+                    <div class="detail-gallery">
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                    </div>
+                </li>
+
+                <li class="adventure">
+                    <div class="detail-info">
+                        <div class="detail-text">
+                            <p class="title">Adventure</p>
+                            <p>Hangout app with adaptive nearby notification and privacy in mind</p>
+                        </div>
+                    </div>
+
+                    <div class="detail-gallery">
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                    </div>
+                </li>
+
+                <li class="newsrooms">
+                    <div class="detail-info">
+                        <div class="detail-logo"></div>
+                        <div class="detail-text">
+                            <p class="title">Newsrooms</p>
+                            <p>News aggregation/recommendation app based on A.I. and natural language processing</p>
+                        </div>
+                    </div>
+
+                    <div class="detail-gallery">
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                    </div>
+                </li>
+
+                <li class="tiaa">
+                    <div class="detail-info">
+                        <div class="detail-logo"></div>
+                        <div class="detail-text">
+                            <p class="title">THISISALLABOUT</p>
+                            <p>An online journalism with data-driven analysis on social issues</p>
+                        </div>
+                    </div>
+
+                    <div class="detail-gallery">
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                        <div class="gallery"></div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="bello-reach">
+            <p>Contact Me</p>
+            <div class="reach-channels">
+                <a href="https://twitter.com/brklynfivefive/" target="_blank">
+                    <div class="icon twitter"></div>
+                </a>
+                <a href="mailto:brklynfivefive@gmail.com">
+                    <div class="icon email"></div>
+                </a>
+            </div>
+        </div>
+
+        <div class="bello-dataload"></div>
+        <div class="bello-bidobido"</div>
+    </div>`;Array.prototype.forEach.call(document.querySelectorAll(".hero-defined"),function(a,s,i){a.classList.add("animate")}),Array.prototype.forEach.call(document.querySelectorAll(".hero-defined p"),function(a,s,i){a.style.animationDelay=0===s?"10ms":2*s*30+"ms"}),window.screen.width<=980&&document.body.setAttribute("banana-type","mobile"),window.addEventListener("scroll",function(a){this.scrollY>=70?document.querySelector(".bello-header").classList.add("floating"):document.querySelector(".bello-header").classList.remove("floating")})}window.$=window.jQuery=t.a}}]);
