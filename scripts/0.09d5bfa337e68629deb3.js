d1646673fe515135627059f71cc94ca5ff3604bf
Todd <todd@Todds-MacBook-Pro.local>
2019-04-25 16:55:44 -0400

    Redesign website

---
+++ scripts/0.09d5bfa337e68629deb3.js
@@ -0,0 +1,159 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{"e+Ya":function(e,t,i){},mka7:function(e,t,i){"use strict";i.r(t),i.d(t,"render_core",function(){return s});i("YEPY"),i("e+Ya");var a=i("lG0c");function s(){Object(a["bind"])(document.body)`
+    <div class="latte-root section__about">
+        <div class="latte-header" status="">
+            <div class="static-header" status="n">
+                <div class="latte-logo">Todd Oh</div>
+            </div>
+            <ul class="latte-menu" status="n">
+                <li class="menu-action open">
+                    <div alt="Navigation menu open icon"></div>
+                </li>
+                <li class="menu-action close">
+                    <div alt="Navigation menu close icon"></div>
+                </li>
+                ${[{id:"nav-section-home",name:"home",path:""},{id:"nav-section-work",name:"work",path:"work"},{id:"nav-section-about",name:"about",path:"about"}].map(e=>`\n                    <li class="sections" data-sectionid="${e.id}"><a href="#${e.path}">${e.name}</a></li>\n                `)}
+            </ul>
+        </div>
+
+        <div class="latte-grid">
+            <div class="lattegrid latte-intro">
+                <p class="subheading">Hello, I'm Todd.</p>
+                <p>I became involved with technology at an early age by founding a startup company at 21. The projects I have worked on over the years are connected with interface design, news aggregation algorithms, and geolocation. At 17 I gave a TED talk at TEDxYouth Seoul, and has since gone on to major in sociology at Yonsei University in Seoul, South Korea.</p>
+                <p>In addition to my startup background, I'm seeking to understand social issues with data engineering and visualization with nonprofit projects. By utilizing my diverse experience, my goal is to contribute to integrate technology into our daily lives, leading to a better understanding between society and technology. I'm looking forward to being a part of a team that aims to make a next bold move.</p>
+            </div>
+
+            <div class="lattegrid latte-skilledu area__introresume">
+                <ul class="skills">
+                    <p class="subheading">Skills</p>
+                    <li>
+                        <p class="title">Javascript — <span class="details">jQuery/ES6, Handlebars.js, React.js, lit-html, Node.js (RESTful API, GraphQL), Streamlined compile via Webpack</span></p>
+                    </li>
+                    <li>
+                        <p class="title">HTML, CSS</p>
+                    </li>
+                    <li>
+                        <p class="title">Python 3 — <span class="details">Natural Language Processing, Text Vectorizing, K-Means Clustering</span></p>
+                    </li>
+                    <li>
+                        <p class="title">Databases — <span class="details">MySQL, MongoDB</span></p>
+                    </li>
+                    <li>
+                        <p class="title">Server — <span class="details">AWS EC2/RDS</span></p>
+                    </li>
+                    <li>
+                        <p class="title">Design Tool — <span class="details">Sketch, Figma</span></p>
+                    </li>
+                    <li>
+                        <p class="title">Other — <span class="details">Git, Sentry.io, Mixpanel</span></p>
+                    </li>
+                    <li>
+                        <p class="title">Languages — <span class="details">Full proficiency in English and Korean, Localization</span></p>
+                    </li>
+                </ul>
+                <ul>
+                    <p class="subheading">Education</p>
+                    <li>
+                        <p class="title">Yonsei University, Seoul, South Korea</p>
+                        <p class="period">B.S. (Sociology), August 2018</p>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="lattegrid latte-experiences area__introresume">
+                <ul>
+                    <p class="subheading">Experiences</p>
+                    <li>
+                        <p class="title">Project Engineer, THISISALLABOUT</p>
+                        <p class="period">Oct 2017 - Present | Greater New York Area</p>
+                        <p class="details">The data-driven journalism project unravels complex issues and agendas by group, timeframe, and key topic using intelligent data analysis and great visualization. I developed a full set of website and back-end including the algorithm that examines a wide range of media in order to create a qualified look at important issues.</p>
+                        <ul class="details">
+                            <li>Implemented natural text processing with HashingVectorizer/word2doc training, and K-Means clustering with MiniBatchKMeans for automatic daily news clustering.</li>
+                            <li>Built a lightweight front-end structure in Node.js.</li>
+                            <li>Designed and developed visualized contents to analyze President Trump’s tweets and key stories in 2017.</li>
+                            <li>Built an in-house editing console and RESTful API endpoints with Node.js/MongoDB.</li>
+                            <li>All source codes published on <a href="https://github.com/fieldofgreentea?tab=repositories" target="_blank">Github (click)</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="title">Project Engineer, South Korean Government Policy Tracker</p>
+                        <p class="period">Aug 2017 - Dec 2017</p>
+                        <ul class="details">
+                            <li>Created a JSON-format data of the government policies</li>
+                            <li>Designed and built the website and visualization in Javascript/CSS</li>
+                            <li>All data and source codes published on <a href="https://github.com/fieldofgreentea/presidentmoon_agenda" target="_blank">Github (click)</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="title">Founder, Toast Labs, LLC.</p>
+                        <p class="period">Apr 2015 - Jul 2017 | Palo Alto, CA</p>
+                        <p class="details">Founded a mobile application startup to create a unique experience in mobile for people’s daily life. It successfully launched products including “#Newsrooms” which provided a solution for journalistic bias by utilizing token weight based news clustering algorithms. As a developer-founder, I lead core product development of the company.</p>
+                        <ul class="details">
+                            <li>Built an adaptive low-power geofence engine on “Adventure” iOS application for tracking user location continuously in Objective-C.</li>
+                            <li>Built an advanced camera and scribble feature for "Adventure" iOS application in Objective-C.</li>
+                            <li>Built a hybrid interface framework for multiple applications in Javascript.</li>
+                            <li>Implemented a natural language processing algorithm using n-gram/tf-idf weight for news clustering back-end in Node.js.</p>
+                            <li>Built a Node.js/MongoDB server running number of API endpoints for a news aggregation service “#Newsrooms”.</li>
+                            <li>Helped the marketing director to boost user interaction using Mixpanel data.</li>
+                            <li>Designed graphical logos and user interfaces for multiple applications.</li>
+                            <li>Some source codes published on <a href="https://github.com/fieldofgreentea?tab=repositories" target="_blank">Github (click)</a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <ul class="additionalexp">
+                    <p class="subheading">Additional experience</p>
+                    <li>
+                        <p class="title">Ars Praxia</p>
+                        <p class="period">Nov 2011 - Feb 2012</p>
+                        <ul class="details">
+                            <li>Semantic network analysis and data cleaning internship</li>
+                            <li>Company website development</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="title">Samsung Electronics Super Rookie Membership</p>
+                        <p class="period">Apr 2012 - Dec 2013</p>
+                        <ul class="details">
+                            <li>An invite-only program for students in STEM and Liberal Arts</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="title">TEDxYouthSeoul 2010</p>
+                        <ul class="details">
+                            <li>A speech about global startup experience</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="title">Startupweekend 2010 Seoul</p>
+                        <ul class="details">
+                            <li>Member of organizing group for the event</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="title">Teaching Assistant (Topics in Sociology (2)), Yonsei University, Seoul, South Korea</p>
+                        <p class="period">Sep 2014 - Dec 2014</p>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="lattegrid latte-contact area__introresume">
+                <ul>
+                    <p class="subheading">Contact</p>
+                    <li class="reach-channels">
+                        <a href="https://twitter.com/fieldofgreentea/" target="_blank">
+                            <div class="icon twitter" alt="twitter icon"></div>
+                        </a>
+                        <a href="https://www.linkedin.com/in/toddoh/" target="_blank">
+                            <div class="icon linkedin" alt="linkedin icon"></div>
+                        </a>
+                        <a href="mailto:hello@toddoh.com">
+                            <div class="icon email" alt="email icon"></div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="latte-dataload"></div>
+        <div class="latte-bidobido"</div>
+    </div>`,window.screen.width<=980?document.body.setAttribute("banana-type","mobile"):document.body.setAttribute("banana-type","");const e=document.querySelectorAll(".latte-menu .menu-action"),t=document.querySelector(".latte-menu"),i=document.querySelector(".latte-header .static-header");e.forEach(e=>{e.addEventListener("click",()=>{"y"==t.getAttribute("status")?(t.setAttribute("status","n"),i.setAttribute("status","n")):(t.setAttribute("status","y"),i.setAttribute("status","y"))})}),i.addEventListener("click",()=>{"y"==t.getAttribute("status")?(t.setAttribute("status","n"),i.setAttribute("status","n")):(t.setAttribute("status","y"),i.setAttribute("status","y"))}),window.addEventListener("scroll",e=>{const t=document.querySelector(".latte-header");window.scrollY>=70?t.setAttribute("status","not_viewport"):t.setAttribute("status","")})}}}]);
